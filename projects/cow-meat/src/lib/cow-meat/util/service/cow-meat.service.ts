c4c3bc190996e2195b9cacad1b0b87e1e5a986d7
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-09-29 01:50:09 +0300

    fix: 🐛 added env variable

--- projects/cow-meat/src/lib/cow-meat/util/service/cow-meat.service.ts
+++ projects/cow-meat/src/lib/cow-meat/util/service/cow-meat.service.ts
@@ -1,29 +1,27 @@
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Inject, Injectable} from '@angular/core';
+import {Inject, Injectable} from "@angular/core";
 import {LocalStorageService} from "ngx-driver";
 import {Observable} from "rxjs";
-import {environment} from "../../../../../environments/environment";
 import {ICow} from "../interfaces";
 
 @Injectable({
-              providedIn: 'root',
+              providedIn: "root",
             })
 export class CowMeatService {
   public header : HttpHeaders = new HttpHeaders;
 
-
   constructor(
-    private readonly _httpClient : HttpClient, @Inject('env') private environment : any,
+    private readonly _httpClient : HttpClient, @Inject("env") private environment : any,
     private readonly _localStorage : LocalStorageService,
   ) {
-    let tokenFromLocalStorage = _localStorage.geItem('token');
+    let tokenFromLocalStorage = _localStorage.geItem("token");
     this.header               = new HttpHeaders().set(
-      'Authorization',
-      'Bearer ' + tokenFromLocalStorage,
+      "Authorization",
+      "Bearer " + tokenFromLocalStorage,
     );
   }
 
   public getCow() : Observable<ICow> {
-    return this._httpClient.get<ICow>(`${environment.api}`, {headers: this.header});
+    return this._httpClient.get<ICow>(`${this.environment.api}`, {headers: this.header});
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import {LocalStorageService} from "ngx-driver";
 export class AppComponent {
   title = 'cow';
   constructor(private readonly _localStorage: LocalStorageService) {
-    _localStorage.setItem('token', '')
+    _localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbGllbiI6eyJpZCI6MSwibmFtZSI6Ik1paGFpIEJ1bmNpIiwiZW1haWwiOm51bGwsInJvbGUiOm51bGwsInBhc3N3b3JkIjoiJDJiJDEzJDE1dWpCbmY2TFF1aURXdy9rdVFkdmVicWxhMk9iYWdHbVl6aUxGUXc4YUJzdHZxdWx5aTlLIiwidG9rZW4iOm51bGx9LCJpYXQiOjE2NjQzOTU5Mzh9.QDlBcJGSg5J_4yWczvjcWvXeRy4jdtw4fTgtUa17dN8')
   }
 }
