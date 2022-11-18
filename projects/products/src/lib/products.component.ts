34c1e3f3e6f7f042a4b90576d4146aaf9f514b0f
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-11-18 23:20:00 +0200

    feat: 🎸 added latest changes

--- projects/products/src/lib/products.component.ts
+++ projects/products/src/lib/products.component.ts
@@ -3,6 +3,10 @@ import {
   OnInit,
   ViewChild
 } from '@angular/core';
+import {
+  FormControl,
+  FormGroup
+} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {Product} from './product.model';
 import {ProductsService} from './products.service';
@@ -14,7 +18,10 @@ import {ProductsService} from './products.service';
 })
 export class ProductsComponent implements OnInit {
   products$ : Observable<Product[]> | undefined;
-
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
   constructor(private _product : ProductsService) { }
 
   ngOnInit() : void {
--- projects/products/src/lib/products.module.ts
+++ projects/products/src/lib/products.module.ts
@@ -1,6 +1,10 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterOutlet} from '@angular/router';
 import {CarouselComponent} from './component/carousel/carousel.component';
@@ -23,10 +27,16 @@ import {ProductsComponent} from './products.component';
     RouterOutlet,
     CommonModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatNativeDateModule
   ],
   exports: [
     ProductsComponent
-  ]
+  ], providers: [
+    MatDatepickerModule,
+  ],
 })
 export class ProductsModule {}
