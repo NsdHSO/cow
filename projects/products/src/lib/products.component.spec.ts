3271ed3085c75dfeabbec6ffa9e3b75bfac910e5
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-11-23 00:49:40 +0200

    test: 💍 product add new product

--- projects/products/src/lib/products.component.spec.ts
+++ projects/products/src/lib/products.component.spec.ts
@@ -1,23 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {environment} from '../../../../src/environments/environment';
+import {ProductsComponent} from './products.component';
+import {ProductsService} from './products.service';
 
-import { ProductsComponent } from './products.component';
-
-describe('ProductsComponent', () => {
-  let component: ProductsComponent;
-  let fixture: ComponentFixture<ProductsComponent>;
-
+fdescribe('ProductsComponent', () => {
+  let component : ProductsComponent;
+  let fixture : ComponentFixture<ProductsComponent>;
+  let productService = jasmine.createSpyObj('ProductsService', ['addNewProduct']);
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ProductsComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {
+          provider: ProductsService,
+          useValue: productService
+        },
+        {
+          provide: 'env',
+          useValue: environment
+        }
+      ]
     })
-    .compileComponents();
-
+      .compileComponents();
+    productService = TestBed.inject(ProductsService) as jasmine.SpyObj<ProductsService>;
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component)
+      .toBeTruthy();
+  });
+  it('should add new product', () => {
+    productService.addNewProduct = jasmine.createSpy().and.returnValue({})
+
+    component.addNewProduct();
+    expect(productService.addNewProduct).toHaveBeenCalled()
   });
 });
