f4538c0ec498e79b9c5ff2c6a91a57970032f648
NSDHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-11-29 01:25:49 +0200

    feat: 🎸 added 2 secondary router

--- projects/cow-meat/src/lib/cow-meat/cow-meat.component.ts
+++ projects/cow-meat/src/lib/cow-meat/cow-meat.component.ts
@@ -1,27 +1,58 @@
 import {
+  AfterViewInit,
   Component,
-  OnInit,
-} from "@angular/core";
+  HostListener,
+  ViewChild
+} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {MatDrawerMode} from '@angular/material/sidenav';
-import {ActivatedRoute} from "@angular/router";
 import {
-  DataSourceMaterialTable,
-  IActionMaterialColumn,
-} from "ngx-liburg";
-import {SpinnerService} from "ngx-liburg-icon";
-import {BehaviorSubject} from "rxjs";
-import {ICow} from "./util/interfaces";
+  MatDrawerMode,
+  MatSidenav
+} from '@angular/material/sidenav';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router
+} from '@angular/router';
+import {
+  BehaviorSubject,
+  filter
+} from 'rxjs';
 
 @Component({
-  selector: "lib-cow-meat",
-  templateUrl: "./cow-meat.component.html",
-  styleUrls: ["./cow-meat.component.scss"],
+  selector: 'lib-cow-meat',
+  templateUrl: './cow-meat.component.html',
+  styleUrls: ['./cow-meat.component.scss']
 })
-export class CowMeatComponent{
-  public isLoading : BehaviorSubject<boolean> | undefined = new BehaviorSubject<boolean>(
-    true);
-  public shouldRun =true;
+export class CowMeatComponent implements AfterViewInit {
   mode = new FormControl('over' as MatDrawerMode);
+  @ViewChild('sidenav') public sidenav : any;
 
+  constructor(
+    private _router : Router,
+    private _route : ActivatedRoute) {}
+
+  ngAfterViewInit() : void {
+    this.closeSidenav();
+    this._router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(console.log)
+  }
+
+  public goToSidenav(sidenav : MatSidenav, path = 'new-cow') : void {
+    sidenav.open();
+    this._router.navigate([{outlets: {sidenav: path}}], {relativeTo: this._route});
+  }
+
+  @HostListener('keyup', ['$event'])
+  public handleKeyDown(event : KeyboardEvent) {
+    if(event.key === 'Escape') {
+      this._router.navigate([{outlets: {sidenav: null}}], {relativeTo: this._route});
+    }
+  }
+
+  private closeSidenav() : void {
+    let url = this._router.url.includes('sidenav');
+    if(url) {
+      this.sidenav.open();
+    }
+  }
 }
